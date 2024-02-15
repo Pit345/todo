bec91e4c97d73995c0e53c093f2e6b823e9b450c
Pit345 <p1312402@gmail.com>
2024-02-15 21:29:19 +0200

    fix form

--- login/forms.py
+++ login/forms.py
@@ -1,13 +1,10 @@
 from django.contrib.auth.models import User
 from django import forms
 
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        help_texts = {'username': None}
-        labels = {"email": "Email"}
-        widgets = {'password': forms.PasswordInput()}
+class SignupForm(forms.Form):
+    username = forms.CharField()
+    email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
 
 class SigninForm(forms.Form):
     username = forms.CharField()
--- login/views.py
+++ login/views.py
@@ -3,6 +3,7 @@ from .forms import SignupForm, SigninForm
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -15,7 +16,12 @@ def signup(request):
     else:
         signup_form = SignupForm(request.POST)
         if signup_form.is_valid():
-            signup_form.save()
+            username = signup_form.cleaned_data['username']
+            email = signup_form.cleaned_data['email']
+            password = signup_form.cleaned_data['password']
+            User.objects.create_user(username=signup_form.cleaned_data['username'], 
+                                     email=signup_form.cleaned_data['email'], 
+                                     password=signup_form.cleaned_data['password'])
         return redirect(reverse('index'))
 
 
@@ -28,6 +34,7 @@ def signin(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        breakpoint()
         if user is not None:
             login(request, user)
             return redirect(reverse('index'))
