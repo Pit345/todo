5f0bd2bfb3583ab713fbd737000a13853b477f1a
Pit345 <p1312402@gmail.com>
2024-02-19 23:35:02 +0200

    add todo search

--- todo/views.py
+++ todo/views.py
@@ -4,22 +4,17 @@ from django.contrib.auth.decorators import login_required
 from login.forms import TodoForm
 from django.urls import reverse
 from django.contrib import messages
+from .forms import SearchForm
 
 # Create your views here.
 
 def index(request):
     todos = Todo.objects.all()
-    return render(request, 'todo/index.html', {'todos': todos})
+    return render(request, 'todo/index.html', {'todos': todos, 'query': SearchForm()})
 
-@login_required
-def create(request):
-    user = request.user
-    if request.method == 'GET':
-        todo_form = TodoForm()
-        return render(request, 'todo/todo_form.html', {'todo_form': todo_form})
-    else:
-        user.todo_set.create(title=request.POST['title'])
-        return redirect(reverse('index'))
+def search(request):
+    todos = Todo.objects.filter(title__contains=request.GET['query'])
+    return render(request, 'todo/todos_search.html', {'todos': todos})
 
 @login_required
 def completed(request, id):
@@ -32,6 +27,16 @@ def completed(request, id):
         todo.completed = False
         todo.save()
         return redirect('index')
+    
+@login_required
+def create(request):
+    user = request.user
+    if request.method == 'GET':
+        todo_form = TodoForm()
+        return render(request, 'todo/todo_form.html', {'todo_form': todo_form})
+    else:
+        user.todo_set.create(title=request.POST['title'])
+        return redirect(reverse('index'))
 
 def delete(request, id):
     Todo.objects.get(id=id).delete()
