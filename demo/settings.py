564600564f18f5f4b46b1354eb951fd96d21053d
Pit345 <p1312402@gmail.com>
2024-02-21 12:56:02 +0200

    smtp setting

--- demo/settings.py
+++ demo/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'p1312402@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
